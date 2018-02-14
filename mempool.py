47c0549279526f683d458f04f61f1e023e9fb10c
Doetoe <doetoe@gmail.com>
2018-02-14 13:24:00 +0100

    minor

--- mempool.py
+++ mempool.py
@@ -16,13 +16,14 @@ Later (if needed) /whichblock(txid)
 """
 
 from flask import Flask, request
+import sqlite3
 import os
 import json
 import sys
 import getopt
 
 mempool = Flask(__name__)
-mempool.transactions = {}
+mempool.transactions = None # sqlite3.connect("")
 
 # should be POST of several forms rather than a json?
 @mempool.route('/pushtx', methods=['GET'])
@@ -46,9 +47,9 @@ def balance():
     return "to be implemented"
     
 if __name__ == '__main__':
-    args, remaining = getopt.getopt(sys.argv[1:], "H:p:h")
-    args = dict(args)
-    if "-h" in args:
+    opt, remaining = getopt.getopt(sys.argv[1:], "H:p:hd:")
+    opt = dict(opt)
+    if "-h" in opt:
         print("""Usage: 
         %s [options]
 
@@ -56,12 +57,15 @@ if __name__ == '__main__':
         -h          show this help
         -H <host>   the host on which to run (default 127.0.0.1)
         -p <port>   the port on which to listen (default 5100)
+        -d <db>     transaction database (default transactions.db)
 
         Note that when host or port are set, the users must be informed, 
         by setting the right MEMPOOL_URL in config.py or passing the URL
         on the command line.
         """ % (os.path.basename(sys.argv[0])))
         sys.exit()
-    host = args.get("-H", "127.0.0.1")
-    port = int(args.get("-p", 5100))
+    host = opt.get("-H", "127.0.0.1")
+    port = int(opt.get("-p", 5100))
+    db = opt.get("-d", "transactions.db")
+    mempool.transactions = sqlite3.connect(db)
     mempool.run(host=host, port=port)
